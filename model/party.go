5d3979e8bc9a63ad22821282953bd8709d78a194
Mauricio Bahamonde <mauricioc.bahamonde@gmail.com>
2019-10-14 09:56:21 -0400

    set null values by default

--- model/party.go
+++ model/party.go
@@ -8,9 +8,9 @@ type Party struct {
         ID                  uint        `json:"Id" gorm:"primary_key"`
         Name                string      `json:"name"`
         Guests              []Guest     `gorm:"foreignkey:PartyRefer"`
-        InvitationId        uint        `json:"invitationId"`
-        InvitationSent      time.Time   `json:"invitationSent"`
-        InvitationOpened    time.Time   `json:"invitationOpened"`
+        InvitationId        *uint       `json:"invitationId"`
+        InvitationSent      *time.Time  `json:"invitationSent"`
+        InvitationOpened    *time.Time  `json:"invitationOpened"`
         IsAttending         bool        `json:"isAttending"`
         Comments            string      `json:"comments"`
 }
