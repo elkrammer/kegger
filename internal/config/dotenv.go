69481ab6054f5dcb3d4b700aea2a1e9eefb31ee2
Mauricio Bahamonde <mauricioc.bahamonde@gmail.com>
2019-10-16 18:32:45 -0400

    refactoring

--- internal/config/dotenv.go
+++ internal/config/dotenv.go
@@ -1,30 +1,31 @@
 package config
 
 import (
-    "os"
-    "log"
-    "github.com/joho/godotenv"
+  "fmt"
+  "github.com/joho/godotenv"
+  "os"
 )
 
 func LoadEnv() {
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Error loading .env file")
-    }
+  err := godotenv.Load()
 
-    if vars := checkVars(); len(vars) != 0 {
-        log.Printf("Error: Mandatory environment variables not set in .env file: %v", vars)
-	}
+  if err != nil {
+    fmt.Println("Error loading .env file")
+  }
+
+  if vars := checkVars(); len(vars) != 0 {
+    fmt.Sprintf("Error: Mandatory environment variables not set in .env file: %v", vars)
+  }
 }
 
 func checkVars() []string {
-	vars := []string{"DB_USERNAME", "DB_PASSWORD", "DB_NAME", "DB_PORT"}
-	missing := []string{}
-	for _, v := range vars {
-		_, set := os.LookupEnv(v)
-		if !set {
-			missing = append(missing, v)
-		}
-	}
-	return missing
+  vars := []string{"DB_USERNAME", "DB_PASSWORD", "DB_NAME", "DB_PORT", "JWT_SECRET"}
+  missing := []string{}
+  for _, v := range vars {
+    _, set := os.LookupEnv(v)
+    if !set {
+      missing = append(missing, v)
+    }
+  }
+  return missing
 }
