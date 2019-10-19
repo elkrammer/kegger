80def87eae969cfffe947c71d31f7a3f0c6c5c24
Mauricio Bahamonde <mauricioc.bahamonde@gmail.com>
2019-10-18 22:33:28 -0400

    formatting

--- route/router.go
+++ route/router.go
@@ -1,51 +1,51 @@
 package route
 
 import (
-  "os"
+    "os"
 
-  "github.com/elkrammer/gorsvp/api"
+    "github.com/elkrammer/gorsvp/api"
 
-  "github.com/dgrijalva/jwt-go"
-  "github.com/labstack/echo"
-  "github.com/labstack/echo/middleware"
+    "github.com/dgrijalva/jwt-go"
+    "github.com/labstack/echo"
+    "github.com/labstack/echo/middleware"
 )
 
 type jwtClaim struct {
-  Name  string `json:"name"`
-  jwt.StandardClaims
+    Name  string `json:"name"`
+    jwt.StandardClaims
 }
 
 func Init() *echo.Echo {
-  e := echo.New()
+    e := echo.New()
 
-  // Middleware
-  e.Use(middleware.Logger())
-  e.Use(middleware.Recover())
+    // Middleware
+    e.Use(middleware.Logger())
+    e.Use(middleware.Recover())
 
-  // Authentication
-  e.POST("/login", api.UserLogin)
-  e.POST("/token", api.GenRefreshToken)
+    // Authentication
+    e.POST("/login", api.UserLogin)
+    e.POST("/token", api.GenRefreshToken)
 
-  // Restricted routes
-  r := e.Group("/api")
-  config := middleware.JWTConfig{
-    Claims:     &jwtClaim{},
-    SigningKey: []byte(os.Getenv("JWT_SECRET")),
-  }
-	r.Use(middleware.JWTWithConfig(config))
+    // Restricted routes
+    r := e.Group("/api")
+    config := middleware.JWTConfig{
+        Claims:     &jwtClaim{},
+        SigningKey: []byte(os.Getenv("JWT_SECRET")),
+    }
+    r.Use(middleware.JWTWithConfig(config))
 
-  // Party Routes
-  r.GET("/party", api.GetParties)
-  r.POST("/party", api.CreateParty)
-  r.GET("/party/:id", api.GetParty)
-  r.PUT("/party/:id", api.UpdateParty)
-  r.DELETE("/party/:id", api.DeleteParty)
+    // Party Routes
+    r.GET("/party", api.GetParties)
+    r.POST("/party", api.CreateParty)
+    r.GET("/party/:id", api.GetParty)
+    r.PUT("/party/:id", api.UpdateParty)
+    r.DELETE("/party/:id", api.DeleteParty)
 
-  // Guest Routes
-  r.GET("/guest/:id", api.GetGuest)
-  r.POST("/guest", api.CreateGuest)
-  r.PUT("/guest/:id", api.UpdateGuest)
-  r.DELETE("/guest/:id", api.DeleteGuest)
+    // Guest Routes
+    r.GET("/guest/:id", api.GetGuest)
+    r.POST("/guest", api.CreateGuest)
+    r.PUT("/guest/:id", api.UpdateGuest)
+    r.DELETE("/guest/:id", api.DeleteGuest)
 
-  return e
+    return e
 }
