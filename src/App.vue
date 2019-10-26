d82051eb725f00720da35befd21487866f2be337
Mauricio Bahamonde <mauricioc.bahamonde@gmail.com>
2019-10-26 19:12:52 -0400

    buefy

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,11 @@
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import "~bulma/sass/utilities/_all";
+// bulma customizations go here
+
+// import Bulma and Buefy styles
+@import "~bulma";
+@import "~buefy/src/scss/buefy";
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import Vuelidate from "vuelidate";
-
+import Buefy from 'buefy'
 import App from './App.vue'
 import router from "./router";
 import store from "./store";
+import 'buefy/dist/buefy.css'
 
+Vue.use(Buefy)
 Vue.use(Vuelidate);
+
 Vue.config.productionTip = false
 
 new Vue({
