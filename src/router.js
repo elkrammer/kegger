6afb89ede2cae64eb83ec3a8e4feb7980547c16c
Mauricio Bahamonde <mauricioc.bahamonde@gmail.com>
2019-10-26 18:54:30 -0400

    login kinda works now

--- src/router.js
+++ src/router.js
@@ -28,4 +28,25 @@ const router = new Router({
   ]
 });
 
+router.beforeEach((to, from, next) => {
+  // set the title of each route
+  document.title = to.meta.title;
+  let accessToken = localStorage.getItem("accessToken") ? localStorage.getItem("accessToken"): null;
+  let refreshToken = localStorage.getItem("refreshToken") ? localStorage.getItem("refreshToken"): null;
+  if (accessToken) {
+    router.app.$options.store.dispatch("user/setUserAndTokens", {
+      accessToken: accessToken,
+      refreshToken: refreshToken
+    });
+  }
+
+  // if the user is not logged in do not allow acess into protected pages.
+  if (to.meta.requiresAuth && !router.app.$options.store.getters["user/user"]) {
+    next({ name: "login" });
+  }
+
+  next();
+
+});
+
 export default router;
--- src/store/user.js
+++ src/store/user.js
@@ -2,23 +2,62 @@ import Vue from "vue";
 import Vuex from "vuex";
 import jwtDecode from "jwt-decode";
 import axios from "@/common/axios";
+import store from "@/store/index";
+import router from "@/router";
+import { setAuthorizationHeader } from "@/common/utilities";
 
 Vue.use(Vuex);
+
+let authTokenRequest;
+async function getAuthToken() {
+  if (!authTokenRequest) {
+    authTokenRequest = store.dispatch("user/refreshUserTokens");
+    authTokenRequest.then(() => {
+      authTokenRequest = null;
+    }).catch(() => {
+      authTokenRequest = null;
+    });
+  }
+}
+
+async function logoutOfApplication() {
+  await store.dispatch("user/userLogout")
+  router.replace({ name: "login" });
+}
 
 // axios interceptor that will handle expired tokens
 axios.interceptors.response.use(undefined, async error => {
   if (error.response.status === 401 && error.response.data.message === "TOKEN_EXPIRED" && !error.config.__isRetryRequest) {
     try {
-      console.log("token expired")
+      let response = await getAuthToken();
+      await store.dispatch("user/setUserAndTokens", {
+        accessToken: response.data.access_token,
+        refreshToken: response.data.refresh_token
+      });
+      error.config.headers["Authorization"] = "Bearer " + store.getters["user/accessToken"];
+      error.config.__isRetryRequest = true;
+      error.config.baseURL = ""; // baseURL needs to be zeroud out to prevent tripping over the baseURL set in main axios instance
+      return axios(error.config);
     } catch (error) {
+      logoutOfApplication();
       return Promise.reject(error);
     }
   }
+
+  // handle user that isn't correctly logged in
+  if (error.response.status === 401 && error.response.data.message === "AUTHENTICATION_ERROR") {
+    logoutOfApplication();
+    return Promise.reject(error);
+  }
+
+  // if someone gets here we don't want to log them out since it's more of a general error
+  return Promise.reject(error);
 });
 
 const SET_USER = "SET_USER";
 const STORE_ACCESS_TOKEN = "STORE_ACCESS_TOKEN";
 const STORE_REFRESH_TOKEN = "STORE_REFRESH_TOKEN";
+const LOGOUT_USER = "LOGOUT_USER";
 
 const user = {
   namespaced: true,
@@ -39,9 +78,24 @@ const user = {
       state.refreshToken = refreshToken
       localStorage.setItem("refreshToken", refreshToken);
     },
+    LOGOUT_USER(state) {
+      state.user = null;
+      state.accessToken = null;
+      state.refreshToken = null;
+      localStorage.removeItem("accessToken");
+      localStorage.removeItem("refreshToken");
+    }
   },
   getters: {
-
+    user(state) {
+      return state.user;
+    },
+    accessToken(state) {
+      return state.accessToken;
+    },
+    refreshToken(state) {
+      return state.refreshToken;
+    }
   },
   actions: {
     async setUserAndTokens({ commit }, data) {
@@ -61,13 +115,31 @@ const user = {
           password: credentials.password
         });
         return await dispatch("setUserAndTokens", {
-          accessToken: response.data.accessToken,
-          refreshToken: response.data.refreshToken
+          accessToken: response.data.access_token,
+          refreshToken: response.data.refresh_token
         });
       } catch (error) {
         return Promise.reject(error.response ? error.response : error);
       }
     },
+    async refreshUserTokens({ getters, rootGetters }) {
+      try {
+        setAuthorizationHeader(rootGetters["user/accessToken"]);
+        return await axios.post("/token", {
+          username: getters.user.username,
+          refreshToken: getters.refreshToken
+        });
+      } catch (error) {
+        return Promise.reject(error.response ? error.response : error);
+      }
+    },
+    async userLogout({ commit }) {
+      try {
+        commit(LOGOUT_USER);
+      } catch (error) {
+        return Promise.reject(error.response ? error.response : error);
+      }
+    }
   }
 }
 
