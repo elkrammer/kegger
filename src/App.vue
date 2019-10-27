3d6f1963fd010114566a250e2d0f344a6400f7c3
Mauricio Bahamonde <mauricioc.bahamonde@gmail.com>
2019-10-27 09:15:33 -0400

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,31 @@
 <template>
   <div id="app">
+    <NavBar v-if="showNavBar" />
     <router-view/>
   </div>
 </template>
+
+<script>
+import NavBar from "@/components/Layout/NavBar";
+
+export default {
+  name: "app",
+  components: {
+    NavBar,
+  },
+  computed: {
+    showNavBar() {
+      return this.$route.name !== 'login';
+    }
+  }
+};
+</script>
 
 <style lang="scss">
 @import "~bulma/sass/utilities/_all";
 // bulma customizations go here
 
-// import Bulma and Buefy styles
+  // import Bulma and Buefy styles
 @import "~bulma";
 @import "~buefy/src/scss/buefy";
 </style>
---
+++ src/components/Layout/NavBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <b-navbar>
+    <template slot="brand">
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        <img
+          src="@/assets/logo-small.png"
+          alt="WingDing Logo"
+          >
+      </b-navbar-item>
+    </template>
+    <template slot="start">
+      <b-navbar-item href="#">
+        Home
+      </b-navbar-item>
+      <b-navbar-item href="#">
+        Parties
+      </b-navbar-item>
+    </template>
+  </b-navbar>
+</template>
+
+<script>
+  export default {
+    name: "navbar",
+  };
+</script>
