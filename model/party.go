abba6eb65783e4e408857ec591e34209519e309a
Mauricio Bahamonde <mauricioc.bahamonde@gmail.com>
2019-11-03 18:50:02 -0500

    add host_id to party model, disable password in User model

--- model/party.go
+++ model/party.go
@@ -8,6 +8,7 @@ type Party struct {
         ID                  uint        `json:"Id" gorm:"primary_key"`
         Name                string      `json:"name"`
         Guests              []Guest     `gorm:"foreignkey:PartyRefer"`
+        HostId              *uint       `gorm:"foreignkey:HostRefer json:"-"`
         InvitationId        *uint       `json:"invitationId"`
         InvitationSent      *time.Time  `json:"invitationSent"`
         InvitationOpened    *time.Time  `json:"invitationOpened"`
--- model/user.go
+++ model/user.go
@@ -1,8 +1,9 @@
 package model
 
 type User struct {
+        HostRefer           uint
         ID                  uint        `json:"Id" gorm:"primary_key"`
         Name                string      `json:"Name"`
         Email               string      `json:"email" gorm:"unique_index"`
-        Password            string      `json:"password"`
+        Password            string      `json:"-"`
 }
