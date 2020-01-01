77d22c35d0deda9fd7301caebb5fb75d18bcae0d
Mauricio Bahamonde <mauricioc.bahamonde@gmail.com>
2020-01-01 16:53:17 -0500

    incorporate logic into invite: set invite opened, set attending, etc

--- src/components/Invites/Invitation.vue
+++ src/components/Invites/Invitation.vue
@@ -1,35 +1,49 @@
 <template>
-  <div class="invite-main" v-if="Object.entries(this.invite).length > 0">
 
-    <div class="title-box">
-      <span>Save the Date!</span>
+  <section class="invite-background">
+    <div class="invite-main" v-if="Object.entries(this.invite).length > 0">
+
+      <div class="title-box">
+        <span>Save the Date!</span>
+      </div>
+
+      <div class="event-date">
+        <span>{{ invite.event_date | moment('timezone', 'America/Toronto', 'MMM do YYYY H:mm') }}</span>
+      </div>
+
+      <div class="invite">
+        <p>
+          Hello {{ this.invite.guest.first_name }}!<br />
+          You've been invited to {{ this.invite.event_name }}.<br /><br />
+        </p>
+        <span v-if="this.invite.guest.plus_one">
+          You are allowed to bring a guest. <br /><br />
+        </span>
+
+        <span>
+          This event will take place at {{ invite.event_location }}. <br /><br />
+          The dress code for this event is {{ invite.dress_code }}. <br />
+        </span>
+      </div>
+
+      <article class="message">
+        <div class="message-header">
+          <p>RSVP</p>
+        </div>
+        <div class="message-body">
+          <b-switch type="is-active" size="is-large" v-model="attending">
+            Are you attending?
+          </b-switch>
+        </div>
+      </article>
+
     </div>
 
-    <div class="event-date">
-      <span>{{ invite.event_date | moment('timezone', 'America/Toronto', 'MMM do YYYY H:mm') }}</span>
+    <div v-else>
+      <h1>Invalid Invitation ID</h1>
     </div>
 
-    <div class="invite">
-      <p>
-        Hello {{ this.invite.guest.first_name }}!<br />
-        You've been invited to {{ this.invite.event_name }}.<br /><br />
-      </p>
-      <span v-if="this.invite.guest.plus_one">
-        You are allowed to bring a guest. <br /><br />
-      </span>
-
-      <span>
-        This event will take place at {{ invite.event_location }}. <br /><br />
-        The dress code for this event is {{ invite.dress_code }}. <br />
-      </span>
-    </div>
-
-  </div>
-
-  <div v-else>
-    <h1>Invalid Invitation ID</h1>
-  </div>
-
+  </section>
 </template>
 
 <script>
@@ -40,7 +54,7 @@ export default {
   props: ['id'],
   data() {
     return {
-
+      attending: false,
     }
   },
   computed: {
@@ -52,10 +66,41 @@ export default {
     async getInvite() {
       try {
         await this.$store.dispatch("invite/getInvite", this.id);
+        if (this.invite && this.invite.guest.invitation_opened == null) {
+          this.setInviteOpened();
+        }
       } catch (error) {
         console.log(error);
       }
     },
+    async setInviteOpened() {
+      try {
+        const data = {
+          action: "opened",
+          invitation_id: this.id
+        };
+        await this.$store.dispatch("invite/updateInvite", data);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async setAttending() {
+      try {
+        const data = {
+          action: "attending",
+          invitation_id: this.id,
+          is_attending: this.attending
+        };
+        await this.$store.dispatch("invite/updateInvite", data);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  watch: {
+    attending: function() {
+      this.setAttending();
+    }
   },
   created() {
     this.getInvite();
@@ -68,18 +113,28 @@ export default {
 @import url('https://fonts.googleapis.com/css?family=Roboto+Condensed');
 @import url('https://db.onlinewebfonts.com/c/19106915702fcec8936aa3e6cf1019aa?family=ConeriaScriptW01-Medium');
 
+.message {
+  text-align: center;
+  margin: auto;
+  margin-top: 30px;
+  width: 30%;
+}
+
 .invite-background {
   background-image: url("~@/assets/invite_bg.jpg");
+  background-color: #3D9970;
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
+  background-color: #3D9970;
 }
 
 .invite-main {
-  height: 4000px;
+  height: 1000px;
 }
 
 .title-box {
+  padding-top: 50px;
   font-family: 'ConeriaScriptW01-Medium', sans-serif;
   font-size: 6.0em;
   color: white;
--- src/store/invite.js
+++ src/store/invite.js
@@ -7,6 +7,7 @@ import { setAuthorizationHeader } from "@/common/utilities";
 Vue.use(Vuex);
 
 const GET_INVITE = "GET_INVITE";
+const UPDATE_INVITE = "UPDATE_INVITE";
 
 const invite = {
   namespaced: true,
@@ -49,6 +50,26 @@ const invite = {
         const response = await axios.get("/invite/" + id);
         commit(GET_INVITE, response.data);
         return response.data;
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    },
+    async updateInvite({ commit }, data) {
+      try {
+        if (data.action === "opened") {
+          var date = new Date();
+          const response = await axios.put("/invite", {
+            invitation_id: data.invitation_id,
+            invitation_opened: date.toISOString()
+          });
+          commit(UPDATE_INVITE, response.data);
+        } else if (data.action === "attending") {
+          const response = await axios.put("/invite", {
+            invitation_id: data.invitation_id,
+            is_attending: data.is_attending
+          });
+          commit(UPDATE_INVITE, response.data);
+        }
       } catch (error) {
         return Promise.reject(error);
       }
