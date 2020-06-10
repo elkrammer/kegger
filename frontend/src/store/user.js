621d05f2ddfafee3190dad27ecd9d6f1dfb65bf5
Mauricio Bahamonde <mauricioc.bahamonde@gmail.com>
2020-06-09 21:50:42 -0400

    rewrite interceptor logic once again

--- frontend/src/store/user.js
+++ frontend/src/store/user.js
@@ -6,79 +6,51 @@ import Vuex from "vuex";
 import jwtDecode from "jwt-decode";
 import axios from "@/common/axios";
 import store from "@/store/index";
-import router from "@/router";
 
 Vue.use(Vuex);
 
-let authTokenRequest;
+let isAlreadyFetchingAccessToken = false;
+let subscribers = [];
 
-function getAuthToken() {
-    // if our currently stored token expires soon
-    if (store.getters['user/tokenExpiration'] - 240 <= (Date.now() / 1000).toFixed(0)) {
-        // if not already requesting a token
-        if (!authTokenRequest) {
-            authTokenRequest = store.dispatch("user/refreshUserTokens");
-            authTokenRequest.then(() => {
-                authTokenRequest = null; // reset authTokenRequest
-            }).catch(err => {
-                console.log("Error in getAuthToken: " + err);
-            });
-            return authTokenRequest;
-        }
-    }
+function onAccessTokenFetched(access_token) {
+    subscribers = subscribers.filter(callback => callback(access_token))
 }
 
-async function logoutOfApplication() {
-    await store.dispatch("user/userLogout")
-    router.replace({ name: "login" });
+function addSubscriber(callback) {
+    subscribers.push(callback)
 }
 
-// axios interceptor that will handle expired tokens
-axios.interceptors.response.use(undefined, async error => {
-    // prevent endless redirects
-    if (error.request !== undefined) {
-        if (error.request.responseURL.includes('login')) {
-            return Promise.reject(error)
+axios.interceptors.response.use(function (response) {
+    return response
+}, function (error) {
+    const { config, response: { status } } = error
+    const originalRequest = config
+
+    if (status === 401 && error.response.data.message === "invalid or expired jwt") {
+        if (!isAlreadyFetchingAccessToken) {
+            isAlreadyFetchingAccessToken = true
+            store.dispatch("user/refreshUserTokens").then((access_token = localStorage.getItem('accessToken')) => {
+                isAlreadyFetchingAccessToken = false
+                onAccessTokenFetched(access_token)
+            })
         }
+
+        const retryOriginalRequest = new Promise((resolve) => {
+            addSubscriber(access_token => {
+                originalRequest.headers.Authorization = 'Bearer ' + access_token
+                resolve(axios(originalRequest))
+            })
+        })
+        return retryOriginalRequest
     }
-
-    if (error.response && error.response.status === 401 &&
-        error.response.data.message === "invalid or expired jwt" &&
-        store.getters['user/retries'] < 3) {
-        try {
-            let response = await getAuthToken();
-
-            if (!response) {
-                store.dispatch("user/increaseRetry");
-                return Promise.reject(error.config);
-            }
-
-            await store.dispatch("user/setUserAndTokens", {
-                accessToken: response.data.access_token,
-                refreshToken: response.data.refresh_token
-            });
-
-            error.config.headers["Authorization"] = "Bearer " + store.getters["user/accessToken"];
-            error.config.baseURL = ""; // baseURL needs to be zeroud out to prevent tripping over the baseURL set in main axios instance
-            return axios.request(error.config);
-            // return axios(error.config);
-        } catch (error) {
-            return
-        }
-    } else if (error.response && error.response.status === 401 && error.response.data.message === "invalid or expired jwt" && store.getters['user/retries'] > 2) {
-        logoutOfApplication();
-        return Promise.reject(error);
-    }
-
-    // if someone gets here we don't want to log them out since it's more of a general error
-    return Promise.reject(error);
-});
+    return Promise.reject(error)
+})
 
 const SET_USER = "SET_USER";
 const STORE_ACCESS_TOKEN = "STORE_ACCESS_TOKEN";
 const STORE_REFRESH_TOKEN = "STORE_REFRESH_TOKEN";
+const STORE_TOKEN_EXPIRATION = "STORE_TOKEN_EXPIRATION";
 const LOGOUT_USER = "LOGOUT_USER";
-const INCREASE_RETRY = 'INCREASE_RETRY';
 
 const user = {
     namespaced: true,
@@ -87,7 +59,6 @@ const user = {
         accessToken: null,
         refreshToken: null,
         tokenExpiration: null,
-        retries: 0,
     },
     mutations: {
         SET_USER(state, data) {
@@ -101,18 +72,18 @@ const user = {
             state.refreshToken = refreshToken
             localStorage.setItem("refreshToken", refreshToken);
         },
+        STORE_TOKEN_EXPIRATION(state, tokenExpiration) {
+            state.tokenExpiration = tokenExpiration
+            localStorage.setItem("tokenExpiration", tokenExpiration);
+        },
         LOGOUT_USER(state) {
             state.user = null;
             state.accessToken = null;
             state.refreshToken = null;
             state.tokenExpiration = null;
-            state.retries = 0;
             localStorage.removeItem("accessToken");
             localStorage.removeItem("refreshToken");
         },
-        INCREASE_RETRY(state) {
-            state.retries += 1
-        }
     },
     getters: {
         user(state) {
@@ -127,9 +98,6 @@ const user = {
         tokenExpiration(state) {
             return state.tokenExpiration;
         },
-        retries(state) {
-            return state.retries;
-        },
     },
     actions: {
         async setUserAndTokens({ commit }, data) {
@@ -138,7 +106,7 @@ const user = {
                 commit(SET_USER, decoded.username);
                 commit(STORE_ACCESS_TOKEN, data.accessToken);
                 commit(STORE_REFRESH_TOKEN, data.refreshToken);
-                this.tokenExpiration = decoded.exp;
+                commit(STORE_TOKEN_EXPIRATION, decoded.exp);
             } catch (error) {
                 return Promise.reject(error);
             }
@@ -177,9 +145,6 @@ const user = {
             } catch (error) {
                 return Promise.reject(error);
             }
-        },
-        increaseRetry({ commit }) {
-            commit(INCREASE_RETRY);
         },
     }
 };
