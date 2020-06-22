18b2b7a640e8c982fc14c69eecc56cdde8608b66
Mauricio Bahamonde <mauricioc.bahamonde@gmail.com>
2020-06-22 17:18:45 -0400

    default invite img

--- api/db/migrations/00005_create_settings_table.up.sql
+++ api/db/migrations/00005_create_settings_table.up.sql
@@ -44,8 +44,8 @@ VALUES('kegger_api_url', 'http://127.0.0.1:4040', 'Kegger API URL');
 -- Invite Settings
 INSERT INTO public.settings
 ("name", value, description)
-VALUES('invite_image_en', '/uploads/default_bg.jpg', 'English Invitation Image Path');
+VALUES('invite_image_en', '/uploads/invite_en.jpg', 'English Invitation Image Path');
 
 INSERT INTO public.settings
 ("name", value, description)
-VALUES('invite_image_es', '/uploads/default_bg.jpg', 'Spanish Invitation Image Path');
+VALUES('invite_image_es', '/uploads/invite_es.jpg', 'Spanish Invitation Image Path');
