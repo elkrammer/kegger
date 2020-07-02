42ea66b8ebd171be4c304643ab5852e4e153ca5a
Mauricio Bahamonde <mauricioc.bahamonde@gmail.com>
2020-07-02 18:56:50 -0400

    add screen to view environment variables

---
+++ frontend/src/components/Settings/Environment.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="box">
+    <h1 class="title">Environment variables</h1>
+
+    <div v-for="(value, name) in settings" :key="name">
+      <b-field :label="name" type="is-info">
+        <b-input :value="value" rounded></b-input>
+      </b-field>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "environment_settings",
+  data() {
+    return {
+      settings: {
+        VUE_APP_API_SERVER: process.env.VUE_APP_API_SERVER,
+        VUE_APP_EMAIL_FROM_NAME: process.env.VUE_APP_EMAIL_FROM_NAME,
+        VUE_APP_EMAIL_FROM: process.env.VUE_APP_EMAIL_FROM,
+        VUE_APP_EMAIL_DEBUG: process.env.VUE_APP_EMAIL_DEBUG,
+        VUE_APP_EMAIL_TO: process.env.VUE_APP_EMAIL_TO
+      },
+    };
+  },
+}
+</script>
--- frontend/src/components/Settings/Settings.vue
+++ frontend/src/components/Settings/Settings.vue
@@ -3,33 +3,37 @@
     <div class="container">
       <h1 class="title">Settings</h1>
 
-        <b-tabs type="is-toggle">
-            <b-tab-item label="Event" icon="calendar-alt">
-              <Event />
-            </b-tab-item>
-            <b-tab-item label="Invite Design" icon="envelope">
-              <Invite />
-            </b-tab-item>
-            <b-tab-item label="App Settings" icon="cog">
-              <App />
-            </b-tab-item>
-            <b-tab-item label="Manage Users" icon="users">
-              <Users />
-            </b-tab-item>
-        </b-tabs>
+      <b-tabs type="is-toggle">
+        <b-tab-item label="Event" icon="calendar-alt">
+          <Event />
+        </b-tab-item>
+        <b-tab-item label="Invite Design" icon="envelope">
+          <Invite />
+        </b-tab-item>
+        <b-tab-item label="App Settings" icon="cog">
+          <App />
+        </b-tab-item>
+        <b-tab-item label="Manage Users" icon="users">
+          <Users />
+        </b-tab-item>
+        <b-tab-item label="Show Env Settings" icon="tree">
+          <Environment />
+        </b-tab-item>
+      </b-tabs>
 
     </div>
   </section>
 </template>
 
 <script>
-  import Users from "@/components/Settings/Users.vue";
-  import Invite from "@/components/Settings/Invite.vue";
-  import Event from "@/components/Settings/Event.vue";
-  import App from "@/components/Settings/App.vue";
+import Users from "@/components/Settings/Users.vue";
+import Invite from "@/components/Settings/Invite.vue";
+import Event from "@/components/Settings/Event.vue";
+import App from "@/components/Settings/App.vue";
+import Environment from "@/components/Settings/Environment.vue";
 
-  export default {
-    name: "Settings",
-    components: { Users, Invite, Event, App },
-  }
+export default {
+  name: "Settings",
+  components: { Users, Invite, Event, App, Environment },
+}
 </script>
